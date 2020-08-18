e712e6c987992c73aa87bc21df1c3b33d3350b86
inink <inink_1997@hotmail.com>
2020-08-18 15:38:28 +0700

    Adding translation posts

---
+++ resources/lang/la/posts.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'posts' => 'ໂພສ',
+    'allPosts' => 'ໂພສທັງຫມົດ',
+    'addNew' => 'ເພີ່ມໂພສ',
+    'editPots' => 'ແກ້ໄຂໂພສ',
+    'updatePots' => 'ອັບເດດໂພສ',
+    'deletePots' => 'ລຶບໂພສ',
+    'view' => 'ເບິ່ງ',
+    'published' => 'ສາທາລະນະ',
+    'draft' => 'ຮ່າງ',
+    'searchPots' => 'ຄົ້ນຫາໂພສ',
+    'categorie' => 'ປະເພດ',
+    'tags' => 'ແທັກ',
+    'date' => 'ວັນທີ',
+    'postAddedSuccessfully' => 'ເພີ່ມໂພສສຳເລັດ'
+
+];
